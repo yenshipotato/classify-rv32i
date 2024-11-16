be5a010456e9f5d3d02fe8010773490c0640ffa0
yenshipotato <jihmjihh@gmail.com>
2024-11-16 13:31:09 +0800

    Add implementation of relu.s

--- src/relu.s
+++ src/relu.s
@@ -29,6 +29,22 @@ relu:
 
 loop_start:
     # TODO: Add your own implementation
+    bge t1, a1, end # if t1 >= a1 then end
+    slli t2, t1, 2 # t2 = t1 * 4
+    add t3, a0, t2 # t3 = a0 + t2
+    lw t4, 0(t3) # t4 = *t3
+    blt t4, zero, if_branch # if t4 < 0 then skip
+    
+else:
+    addi t1, t1, 1 # t1 = t1 + 1
+    j loop_start # jump to loop_start
+
+if_branch:
+    sw zero, 0(t3) # *t3 = 0
+    j else # jump to else
+
+end:    
+    jr ra
 
 error:
     li a0, 36          
