06ccbc8c92fe5634c43d44b3283dc51e68729055
yenshipotato <jihmjihh@gmail.com>
2024-11-16 14:19:17 +0800

    Add implementation of argmax.s

--- src/argmax.s
+++ src/argmax.s
@@ -31,6 +31,24 @@ argmax:
     li t2, 1
 loop_start:
     # TODO: Add your own implementation
+    bge t2, a1, end # if t2 >= a1 then end
+    slli t3, t2, 2 # t3 = t2 * 4
+    add t3, a0, t3 # t3 = a0 + t3
+    lw t4, 0(t3) # t4 = *t3
+
+    ble t4, t0, skip # if t4 <= t0 then skip
+
+set_max:
+    mv t0, t4 # t0 = t4
+    mv t1, t2 # t1 = t2
+
+skip:
+    addi t2, t2, 1 # t2 = t2 + 1
+    j loop_start # jump to loop_start
+
+end:
+    mv a0, t1 # a0 = t1
+    jr ra
 
 handle_error:
     li a0, 36
